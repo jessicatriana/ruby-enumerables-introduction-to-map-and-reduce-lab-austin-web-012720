d49d7efa86fec8774a962a7f6737a9a0a5cf17fb
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-31 02:49:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,10 +54,9 @@ end
 def reduce_to_all_true(array) 
   index = 0 
   while index < array.length
-  if array[index] === true 
-    return true 
-    index += 1 
-  else
+ return false if array[index] == false
     return false
+    index += 1 
+ 
   end
 end
