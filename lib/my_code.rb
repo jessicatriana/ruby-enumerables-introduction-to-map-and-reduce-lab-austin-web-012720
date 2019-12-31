b5362746f2c209a3c288d03126d0ac74b97e0489
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-31 02:14:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,53 @@
+require 'pry'
+
 # My Code here....
+def map_to_negativize(array)
+  new_array = []
+  index = 0 
+  while index < array.length do
+ new_array << array[index] * -1
+ index += 1
+end
+  new_array
+end
+
+def map_to_no_change(array)
+  new_array = []
+  index = 0 
+  while index < array.length
+  new_array = array
+  index += 1 
+end 
+new_array 
+end
+
+def map_to_double(array)
+  new_array = [] 
+  index = 0 
+  while index < array.length 
+  new_array << array[index] * 2
+  index += 1 
+end
+new_array 
+end
+
+def map_to_square(array) 
+  new_array = []
+  index = 0 
+  while index < array.length  
+  new_array << array[index] **2
+  index += 1 
+end
+new_array 
+end
+
+def reduce_to_total(array, 0)
+  total = 0
+  index = 0
+
+ while index < array.length
+    total = total + array[index]
+    index += 1
+end
+  total
+end
